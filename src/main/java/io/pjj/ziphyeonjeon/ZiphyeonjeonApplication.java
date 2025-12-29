352660161b79e65b546c349c0e330c2d86947b4b
Jihoon\ghdah <wjdwlgns11q@gmail.com>
2025-12-29 20:43:45 +0900

    Initial commit

---
+++ src/main/java/io/pjj/ziphyeonjeon/ZiphyeonjeonApplication.java
@@ -0,0 +1,13 @@
+package io.pjj.ziphyeonjeon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZiphyeonjeonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZiphyeonjeonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/pjj/ziphyeonjeon/ZiphyeonjeonApplicationTests.java
@@ -0,0 +1,13 @@
+package io.pjj.ziphyeonjeon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZiphyeonjeonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
