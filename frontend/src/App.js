4155d67b892f281abeef016edb502f61197db973
정지훈 <wjdwlgns11q@gmail.com>
2026-01-02 17:04:21 +0900

    react router 연결

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,25 @@
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import RiskAnalysis from "./RiskAnalysis/RiskAnalysis";
+
+// router 설정 페이지입니다. 여기서 메인페이지 만들지 마세요
+// 그냥 라우터 연결만!!
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          실행 확인
-        </p>
-      </header>
-    </div>
-  );
+    return (
+        <BrowserRouter>
+            <nav>
+                <Link to={"/"}><div>홈 화면</div></Link>
+                <Link to="/RiskAnalysis">위험 분석 페이지로 이동</Link>
+            </nav>
+
+            <Routes>
+                <Route path="/" element={<h2>홈 화면</h2>} />
+
+                {/*RiskAnalysis router*/}
+                <Route path="/RiskAnalysis" element={<RiskAnalysis />} />
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
 export default App;
---
+++ frontend/src/RiskAnalysis/RiskAnalysis.js
@@ -0,0 +1,5 @@
+function RiskAnalysis() {
+    return <h2>RiskAnalysis.js 페이지</h2>
+}
+
+export default RiskAnalysis;
